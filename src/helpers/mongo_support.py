34e95fd191c5d0ddf928ef0ba9fd3093ffd226fa
Sami Hakkarainen <samihakkarainen@Samis-MacBook-Air.local>
2024-08-13 21:21:36 -0700

    change uri to env variable

--- src/helpers/mongo_support.py
+++ src/helpers/mongo_support.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import os
 
 class MongoConnect:
     def __init__(self) -> None:
         pass
     def connect_collecion(self):
-        uri = "mongodb+srv://shakka32:chickendinner@nfl.uvhq9b9.mongodb.net/?tls=true&tlsAllowInvalidCertificates=true&retryWrites=true&w=majority&appName=NFL"
+        uri = uri = os.getenv('MONGODB_URI')
+        
+        if not uri:
+            raise ValueError("MONGODB_URI environment variable not set")
+
         client = MongoClient(uri, server_api=ServerApi('1'))
         # Send a ping to confirm a successful connection
         try:
